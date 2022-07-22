9bf44473565a8f8bf2e62b8869346e97f8435de6
kamal76k <109798859+kamal76k@users.noreply.github.com>
2022-07-22 16:15:58 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,53 @@
+#first program in python
+
+ # print("Hello kamal")
+
+
+# second program in python
+
+# a=int(input("Enter the first number:\n"))
+#b=int(input("Enter the second number:\n"))
+#c=a+b
+#print("The add of two number is =",+c)
+
+# Third program in python
+
+# x=int(input('Enter the first number :\n'))
+#y=int(input("Enter the second number:\n"))
+
+#multiply=x*y
+#print('The multiply of x and b is =',multiply)
+
+
+#Datatype in python
+
+a=4
+print(type(a))
+
+b='kamal'
+print(type(b))
+
+c="kamal"
+print(type(c))
+
+x=["kamal","Amit","sagar"]
+print(type(x))
+
+y=('apple','banana','oranage')
+print(type(y))
+
+z=1j
+print(type(z))
+
+d=range(6)
+print(type(d))
+
+e=None
+print(type(e))
+
+f={"name":"kamal","age":36}
+print(type(f))
+
+g={"kamal","sagar"}
+print(type(g))
+
